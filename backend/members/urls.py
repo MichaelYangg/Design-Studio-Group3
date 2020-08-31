7343d926585b114412bbe8dd21df3de074a71780
MichaelYangg <50360179+MichaelYangg@users.noreply.github.com>
2020-08-31 17:51:27 +0800

    Update urls.py

--- backend/members/urls.py
+++ backend/members/urls.py
@@ -5,5 +5,7 @@ app_name = 'members'
 urlpatterns = [
     path('', views.overview, name='index'),
     path('<int:phone_number>/',views.detail,name='detail'),
-    path('deposit/<int:phone_number>/',views.deposit,name='deposit')
+    path('deposit/<int:phone_number>/',views.deposit,name='deposit'),
+    path('add_member/',views.add_member,name='add'),
+    path('delete_member/',views.delete_member,name='delete')
 ]
