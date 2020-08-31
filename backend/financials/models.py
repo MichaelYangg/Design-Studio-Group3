c83f05de62cfedd6c3a4bbd8727ade815558a73b
MichaelYangg <50360179+MichaelYangg@users.noreply.github.com>
2020-08-31 23:07:30 +0800

    Update models.py

--- backend/financials/models.py
+++ backend/financials/models.py
@@ -3,7 +3,7 @@ import datetime
 # Create your models here.
 
 
-class transaction(models.Model):  # 创建“交易”数据表，包括收银0、储值1、入库2、出库3、加工费用4、盘亏盘盈5、外卖6、其他7的各种交易，用于每日直接成本核算
+class Transaction(models.Model):  # 创建“交易”数据表，包括收银0、储值1、入库2、出库3、加工费用4、盘亏盘盈5、外卖6、其他7的各种交易，用于每日直接成本核算
     transaction_id = models.BigIntegerField(primary_key=True)
     volume = models.FloatField(default=0.0)
     unit = models.CharField(max_length=10)
