ffe3e72440ee1a588f6074796071b8afe6ab5b67
MichaelYangg <50360179+MichaelYangg@users.noreply.github.com>
2020-08-31 22:48:14 +0800

    Update views.py

--- backend/financials/views.py
+++ backend/financials/views.py
@@ -7,15 +7,9 @@ import json
 
 def daily_net_change(request):  # 目前只能手动进行直接成本核算，无法定时自动核算
     today = datetime.date.today()      # 获取当前日期
-<<<<<<< HEAD
-    today_cost = Transaction.objects.filter(time_date__startswith=today, unit='元', volume__lt=0).values('volume')
-    # 取出今天发生的，单位为元，数量为负数的交易（单位为元的交易即金钱交易，货物交易则不包含在内；数量为负数则代表是成本）
-    # today_cost的数据类型为i个字典，i = 今天发生的金钱交易次数
-=======
-    category_list = transaction.objects.values('category').distinct()
+    category_list = Transaction.objects.values('category').distinct()
     # 提取出无重复值的列表，为transaction表中各种不同的category（财务、白菜、猪肉等，今日有多少种不同的transaction就包含多少个）
     # category_list的数据类型为i个字典，i = 不同的category数量
->>>>>>> e7d975baff82d6219ad9f72b40f93ab00042fd2a
 
     today_net_change = {}
     for category_dict in category_list:
