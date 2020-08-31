54f205a5684a1263fa2d191dd8125eb12d03b4e7
MichaelYangg <50360179+MichaelYangg@users.noreply.github.com>
2020-08-31 17:51:24 +0800

    Update views.py

--- backend/members/views.py
+++ backend/members/views.py
@@ -35,13 +35,34 @@ def detail(request, phone_number):
         member_info['Level'] = member.member_class
         return JsonResponse(member_info)
 
+def add_member(request):
+    # member_info = request.GET
+    member_info = {'name': 'yang', 'phone':'123456'}
+    first_name = member_info['name']
+    phone_number = member_info['phone']
+    Member.objects.create(phone=phone_number,first_name=first_name)
+    # 这里有一些问题，首先前端传回的手机号最好应该是BigInt的格式
+    # 第二名字需要确认last name/first name
+    # 第三其他的数据怎么补充
+    result = 'success'
+    return JsonResponse({'result':result})
+
+def delete_member(request):
+    # member_info = request.GET
+    member_info = {'phone':'123456'}
+    mem = Member.objects.get(phone=member_info['phone'])
+    mem.delete()
+    # 这里不需要姓名
+    result = 'success'
+    return JsonResponse({'result':result})
+
 def deposit(request,phone_number):
     try:
         member = Member.objects.get(phone=phone_number)
     except Member.DoesNotExist:
         raise Http404("Member does not exist")
     else:
-        # deposit_amount = request.GET['deposit'] # 待和前端确认修改
+        # deposit_amount = request.GET['balance'] # 待和前端确认修改
         deposit_amount = 10
         member.balance += deposit_amount
         member.credit += deposit_amount
