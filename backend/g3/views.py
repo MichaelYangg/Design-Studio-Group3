622304ff6ca385ee1687428bdadf3a18d5f66cef
MichaelYangg <50360179+MichaelYangg@users.noreply.github.com>
2020-08-31 22:52:14 +0800

    Update views.py

--- backend/g3/views.py
+++ backend/g3/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse, Http404
+from client_management.models import Member
+from financials.models import Transaction
 
 # Create your views here.
+
+def other_cost(request):
+    cost = request.POST
+    current_id = Transaction.objects.order_by('-transaction_id')[:3]
+    
