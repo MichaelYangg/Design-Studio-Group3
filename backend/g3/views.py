92df979e85da18cf2799f955073ceb7964870913
MichaelYangg <50360179+MichaelYangg@users.noreply.github.com>
2020-09-01 09:48:46 +0800

    Update views.py

--- backend/g3/views.py
+++ backend/g3/views.py
@@ -7,5 +7,6 @@ from financials.models import Transaction
 
 def other_cost(request):
     cost = request.POST
-    current_id = Transaction.objects.order_by('-transaction_id')[:3]
-    
+    current_tran = Transaction.objects.order_by('-time_date')[0]
+    current_id = current_tran.transaction_id
+    new_id = current_id + 1
