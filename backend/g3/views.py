d26acb8ac301c3d11d8ce8b7b30b466776613a2a
MichaelYangg <50360179+MichaelYangg@users.noreply.github.com>
2020-09-02 16:06:16 +0800

    Update views.py

--- backend/g3/views.py
+++ backend/g3/views.py
@@ -2,11 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse, Http404
 from client_management.models import Member
 from financials.models import Transaction
+import xmltodict
 
 # Create your views here.
 
 def other_cost(request):
-    cost = request.POST
+    xmlinfo = request.POST
+    data = xmltodict.parse(xmlinfo)
+    cost = data['cost']
     current_tran = Transaction.objects.order_by('-time_date')[0]
     current_id = current_tran.transaction_id
     new_id = current_id + 1
+    try:
+        Transaction.objects.create(transaction_id=new_id,volume=cost,unit='元',resource=4,category='财务',explanation='无')
+        result = 'success'
+    except:
+        result = 'fail'
+    return JsonResponse({'result':'success'})
