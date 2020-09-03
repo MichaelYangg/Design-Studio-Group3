fb801b24b1457c96f1b66e3b7ad177ea06873963
MichaelYangg <50360179+MichaelYangg@users.noreply.github.com>
2020-09-03 23:54:24 +0800

    Update views.py

--- backend/transaction/views.py
+++ backend/transaction/views.py
@@ -41,8 +41,37 @@ def add_transaction(request):
         result = 'fail'
     except:
         try:
-            Transaction.objects.create(transaction_id=transaction_id,volume=tran_info['volume'])
+            Transaction.objects.create(transaction_id=transaction_id,volume=tran_info['volume'],
+                        unit=tran_info['unit'],time_date=tran_info['time_date'],
+                        resource=tran_info['resource'],category=tran_info['category'],
+                        explanation=tran_info['explanation'])
             result = 'success'
         except:
             result = 'fail'
     return JsonResponse({'result':result})
+
+def delete_transaction(request):
+    transaction_id = request.POST['transaction_id']
+    try:
+        tran = Transaction.objects.get(transaction_id=transaction_id)
+        tran.delete()
+        result = 'success'
+    except:
+        result = 'fail'
+    return JsonResponse({'result':result})
+
+def change_transaction(request):
+    tran_info = request.POST['list'][0]
+    try:
+        tran = Transaction.objects.get(transaction_id=tran_info['transaction_id'])
+    except Transaction.DoesNotExist:
+        raise Http404("Transaction does not exist")
+    else:
+        Transaction.objects.filter(transaction_id = tran_info['transaction_id']).update(volume=tran_info['volume'])
+        Transaction.objects.filter(transaction_id = tran_info['transaction_id']).update(unit=tran_info['unit'])
+        Transaction.objects.filter(transaction_id = tran_info['transaction_id']).update(time_date=tran_info['time_date'])
+        Transaction.objects.filter(transaction_id = tran_info['transaction_id']).update(resource=tran_info['resource'])
+        Transaction.objects.filter(transaction_id = tran_info['transaction_id']).update(category=tran_info['category'])
+        Transaction.objects.filter(transaction_id = tran_info['transaction_id']).update(explanation=tran_info['explanation'])
+        result = 'success'
+        return JsonResponse({'result':result})
