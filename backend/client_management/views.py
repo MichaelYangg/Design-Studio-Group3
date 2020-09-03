9e872edece64a3c411c9639a06e085aed5e278da
Yinuo Sheng <shengyinuo@163.com>
2020-09-03 15:26:37 +0800

    Update views.py

--- backend/client_management/views.py
+++ backend/client_management/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from client_management.models import discount_policy
+from client_management.models import DiscountPolicy
 from client_management.models import Member
 from django.core import serializers
 import datetime
@@ -9,9 +9,9 @@ import json
 
 
 def discount(request):
-    data = discount_policy.objects.all()                           # discount_policy是存储优惠信息的数据表
+    data = DiscountPolicy.objects.all()                           
     length = data.count()                                          # 获取表数据总长度
-    result = discount_policy.objects.filter(policy_id=length - 1)  # 取出最后一行（即最新）数据
+    result = DiscountPolicy.objects.filter(policy_id=length - 1)   # 取出最后一行（即最新）数据
     result_start = result.values('start_date')[0]['start_date']    # 取出这行数据的开始日期
     result_end = result.values('end_date')[0]['end_date']          # 取出这行数据的截止日期
     today = datetime.date.today()
@@ -26,8 +26,10 @@ def discount(request):
 
 
 def add_credit(request):  # 会员消费增加积分
-    consumption = 100     # 此数值目前仅用于测试，日后需要修改，应为计算出的用户应付的价格
-    phone = 12345678900   # 此数值目前仅用于测试，日后需要修改，应为用户电话号码
+    # add_credit_info = request.GET
+    add_credit_info = {'volume': 100, 'phone': 12345678900}  # 此数值仅用于测试
+    consumption = add_credit_info['volume']
+    phone = add_credit_info['phone']
     try:
         target = Member.objects.filter(phone=phone)
         original_credit = target.values('credit')[0]['credit']
