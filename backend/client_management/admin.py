fd59b38d860032f02d9f2d985481df873af9e950
MichaelYangg <50360179+MichaelYangg@users.noreply.github.com>
2020-09-03 19:38:48 +0800

    Update admin.py

--- backend/client_management/admin.py
+++ backend/client_management/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 
 # Register your models here.
 from client_management import models
-admin.site.register(models.discount_policy)
+admin.site.register(models.DiscountPolicy)
 admin.site.register(models.Member)
