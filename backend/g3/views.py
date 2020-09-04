93d19444dbd1bae95739f7e73ac5c5ddbbe366b4
Yinuo Sheng <shengyinuo@163.com>
2020-09-04 19:34:57 +0800

    Update views.py

--- backend/g3/views.py
+++ backend/g3/views.py
@@ -19,3 +19,26 @@ def other_cost(request):
     except:
         result = 'fail'
     return JsonResponse({'result':'success'})
+
+
+def payment_done_add_credit(request):
+    # add_credit_info = request.GET
+    payment = {'method': 'cash', 'discount_price': 100, 'time': '2020-09-04 19:00:00', 'phone': 12345678900}  # 此数值仅用于测试，具体看G1的字段名
+    volume = payment['discount_price']
+    phone = payment['phone']
+
+    try:
+        target = Member.objects.filter(phone=phone)
+    finally:
+        original_credit = target.values('credit')[0]['credit']
+        new_credit = original_credit + volume
+        Member.objects.filter(phone=phone).update(credit=new_credit)
+        if original_credit < 1000 and new_credit >= 1000:
+            Member.objects.filter(phone=phone).update(member_class=2)
+            Member.objects.filter(phone=phone).update(discount=0.85)
+        if original_credit < 2000 and new_credit >= 2000:
+            Member.objects.filter(phone=phone).update(member_class=3)
+            Member.objects.filter(phone=phone).update(discount=0.75)
+
+
+    return JsonResponse({'result':'success'})
