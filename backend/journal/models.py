f640e70b0664da8137af6f9c8a20d5fc32e0d3cf
MichaelYangg <50360179+MichaelYangg@users.noreply.github.com>
2020-09-07 11:20:27 +0800

    Revert "Merge branch 'master' of https://github.com/MichaelYangg/Design-Studio-Group3"
    This reverts commit 2f77692923f7140694e91466471d39278e3fe71b, reversing
    changes made to 898a2c9551adadafddd372b7911a918751929544.

--- backend/journal/models.py
+++ backend/journal/models.py
@@ -3,32 +3,12 @@ from django.db import models
 # Create your models here.
 class Daily(models.Model):
     ACCOUNT_CHOICE = (
-        ('财务','财务'),
-        ('白菜','白菜'),
-        ('猪肉','猪肉'),
-        ('生菜','生菜'),
-        ('菜心','菜心'),
-        ('羊肉','羊肉'),
-        ('牛肉','牛肉'),
-        ('醋','醋'),
-        ('白糖','白糖'),
-        ('盐','盐'),
-        ('大米','大米'),
-        ('小米','小米'),
-        ('糯米','糯米'),
-        ('调和油','调和油'),
-        ('菜油','菜油'),
-        ('酱油','酱油'),
-        ('面粉','面粉'),
-        ('米粉','米粉'),
-        ('挂面','挂面'),
-        ('黄豆','黄豆'),
-        ('香菇','香菇'),
-        ('海带','海带'),
+        ('P', 'Profit'),
+        ('L', 'Loss'),
     )
     account_type = models.CharField(max_length=10, choices=ACCOUNT_CHOICE) 
     net_profit = models.FloatField()
-    unit = models.CharField(max_length=50) 
+    unit = models.CharField(max_length=50) #yuan
     date = models.DateField(auto_now=False, auto_now_add=False)
     balance = models.FloatField()
 
