034f6b0809d417c58089e78424287e8bdf96c7cc
MichaelYangg <50360179+MichaelYangg@users.noreply.github.com>
2020-09-08 23:43:41 +0800

    Update settings.py

--- backend/group3/settings.py
+++ backend/group3/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ssosh3yu5(3z9neaobe4@0i2rzkacmremws$^rvxgfa0mah(y8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -81,23 +81,23 @@ WSGI_APPLICATION = 'group3.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql', # 数据库引擎
-#         'NAME': 'group3', # 数据库名
-#         'USER': 'root', # 账号
-#         'PASSWORD': 'sem123', # 密码
-#         'HOST': '127.0.0.1', # HOST
-#         'PORT': 8083, # 端口
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', # 数据库引擎
+        'NAME': 'group3', # 数据库名
+        'USER': 'root', # 账号
+        'PASSWORD': 'sem123', # 密码
+        'HOST': '127.0.0.1', # HOST
+        'PORT': 3306, # 端口
+    }
+}
 
 
 # Password validation
