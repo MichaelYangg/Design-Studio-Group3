d9962e9d29e2e85db98f5cbf1ebcdfa22ed3edb9
MichaelYangg <50360179+MichaelYangg@users.noreply.github.com>
2020-09-10 17:08:57 +0800

    Update urls.py

--- backend/g3/urls.py
+++ backend/g3/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'g3'
 urlpatterns = [
-    path('order_other_cost/', views.other_cost, name='other_cost'),
+    path('order_other_cost', views.other_cost, name='other_cost'),
     path('confirm_payment/', views.payment_done_add_credit, name='payment'),
     path('stock_in/',views.stock_in,name='stock_in'),
     path('stock_out/',views.stock_out,name='stock_out'),
